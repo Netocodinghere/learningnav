e692fa52a168fd32730b70194552c600379c10fc
Neto <nanierobi@gmail.com>
2025-07-11 19:39:44 +0100

    ...

--- app/page.js
+++ app/page.js
@@ -1,13 +1,29 @@
+"use client"
 import { useEffect, useState } from "react";
 import Hero from "./_components/Hero";
+import Dashboard from "./_components/Dashboard";
 export default function Home() {
   const [auth,setAuth] = useState(false)
+  const [user,setUser]=useState(null)
   useEffect(()=>{
     
+    const fetchUser = async () => {
+
+      const { data: { session } } = await supabase.auth.getSession()
+      setUser(session?.user || null)
+     
+      if(session?.user){
+        setAuth(true)
+        
+      }
+ 
+    }
+    fetchUser()
   })
   return (
    <div>
-    <Hero/>
+
+    {auth? <Dashboard/>:<Hero/>}
    </div>
   );
 }
